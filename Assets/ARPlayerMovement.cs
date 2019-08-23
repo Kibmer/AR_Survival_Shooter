64d575a9cfea1269c5d68d1a46c479854998c62c
Kibmer <kkb9243@naver.com>
2019-08-23 16:17:22 +0900

    Add NavMeshComponent, Character movement

---
+++ Assets/ARPlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPlayerMovement : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent nav;
+    public GameObject moveTarget;
+
+    float camRayLength = 100f;
+    int floorMask;
+
+    private void Awake() {
+        floorMask = LayerMask.GetMask ("Floor");
+        Debug.Log(floorMask);
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit floorHit;
+            
+            if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+            {
+                moveTarget.transform.position = floorHit.point;
+                
+            }
+        }
+        nav.SetDestination(moveTarget.transform.position);
+    }
+}
--- Assets/CompleteSurvival/_Complete-Game/Scripts/Player/PlayerMovement.cs
+++ Assets/CompleteSurvival/_Complete-Game/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnitySampleAssets.CrossPlatformInput;
 
 namespace CompleteProject
 {
     public class PlayerMovement : MonoBehaviour
     {
-        public float speed = 6f;            // The speed that the player will move at.
-
+        public float speed = 6f;           // The speed that the player will move at.
+        UnityEngine.AI.NavMeshAgent nav;
+        public Transform moveTarget;
 
         Vector3 movement;                   // The vector to store the direction of the player's movement.
         Animator anim;                      // Reference to the animator component.
@@ -16,73 +18,92 @@ namespace CompleteProject
         float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 #endif
 
-        void Awake ()
+        void Awake()
         {
 #if !MOBILE_INPUT
             // Create a layer mask for the floor layer.
-            floorMask = LayerMask.GetMask ("Floor");
+            floorMask = LayerMask.GetMask("Floor");
 #endif
+            nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
             // Set up references.
-            anim = GetComponent <Animator> ();
-            playerRigidbody = GetComponent <Rigidbody> ();
+            anim = GetComponent<Animator>();
+            playerRigidbody = GetComponent<Rigidbody>();
         }
 
 
-        void FixedUpdate ()
+        void FixedUpdate()
         {
             // Store the input axes.
             float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
             float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
-
+            bool isMoving;
             // Move the player around the scene.
-            Move (h, v);
+            Move(h, v);
 
             // Turn the player to face the mouse cursor.
-            Turning ();
+            // Turning();
+            ClickToMove(out isMoving);
 
             // Animate the player.
-            Animating (h, v);
+            Animating(h, v);
         }
 
-
-        void Move (float h, float v)
+        private void ClickToMove(out bool isMoving)
         {
-            // Set the movement vector based on the axis input.
-            movement.Set (h, 0f, v);
-            
-            // Normalise the movement vector and make it proportional to the speed per second.
-            movement = movement.normalized * speed * Time.deltaTime;
-
-            // Move the player to it's current position plus the movement.
-            playerRigidbody.MovePosition (transform.position + movement);
+            isMoving = false;
+            if(Input.GetMouseButtonDown(0)){
+                isMoving = true;
+                MoveToPoint(Input.mousePosition);
+            }
+            if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+                isMoving = true;
+                MoveToPoint(Input.GetTouch(0).position);
+            }
         }
 
-
-        void Turning ()
+        private void MoveToPoint(Vector3 _target)
         {
-#if !MOBILE_INPUT
-            // Create a ray from the mouse cursor on screen in the direction of the camera.
-            Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-            // Create a RaycastHit variable to store information about what was hit by the ray.
+            Ray camRay = Camera.main.ScreenPointToRay(_target);
             RaycastHit floorHit;
 
-            // Perform the raycast and if it hits something on the floor layer...
-            if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+            if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
             {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-                Vector3 playerToMouse = floorHit.point - transform.position;
-
-                // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
-
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation (playerToMouse);
-
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation (newRotatation);
+                moveTarget.position = floorHit.point;
             }
+        }
+
+
+        void Move(float h, float v)
+        {
+            // // Set the movement vector based on the axis input.
+            // movement.Set(h, 0f, v);
+
+            // // Normalise the movement vector and make it proportional to the speed per second.
+            // movement = movement.normalized * speed * Time.deltaTime;
+
+            // // Move the player to it's current position plus the movement.
+            // playerRigidbody.MovePosition(transform.position + movement);
+            nav.SetDestination(moveTarget.position);
+        }
+
+
+        void Turning()
+        {
+#if !MOBILE_INPUT
+            if (Input.GetMouseButton(0))
+            {
+                Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit floorHit;
+
+                if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+                {
+                    moveTarget.transform.position = floorHit.point;
+
+                }
+            }
+            nav.SetDestination(moveTarget.transform.position);
+        }
 #else
 
             Vector3 turnDir = new Vector3(CrossPlatformInputManager.GetAxisRaw("Mouse X") , 0f , CrossPlatformInputManager.GetAxisRaw("Mouse Y"));
@@ -102,16 +123,17 @@ namespace CompleteProject
                 playerRigidbody.MoveRotation(newRotatation);
             }
 #endif
-        }
 
 
-        void Animating (float h, float v)
+
+
+        void Animating(float h, float v)
         {
             // Create a boolean that is true if either of the input axes is non-zero.
             bool walking = h != 0f || v != 0f;
 
             // Tell the animator whether or not the player is walking.
-            anim.SetBool ("IsWalking", walking);
+            anim.SetBool("IsWalking", walking);
         }
     }
 }
